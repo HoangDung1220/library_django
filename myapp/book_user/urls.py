75d204a358dbcf7ce567872fc62e31656b5177ad
Quốc Huy <lunladoithu284@gmail.comgit config --global user.name quochuy54clear>
2022-05-31 12:23:21 +0700

    model, get and pagenation book

--- myapp/book_user/urls.py
+++ myapp/book_user/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('home/', views.index,name="index" ),
-    path('blog/',views.blog,name='blog')
+    path('blog/',views.blog,name='blog'),
+    path('books/search', views.BooksViewSearch.as_view(), name= 'search'),
+    path('books/',views.BooksView.as_view(),name='books'),
 ]
--- myapp/book_user/views.py
+++ myapp/book_user/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.views import View
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+from .models import Book
 
 
 def index(request):
@@ -8,3 +11,40 @@ def index(request):
 
 def blog(request):
     return render(request,'book_user/blog.html')
+
+# def books(req):
+#     books = Book.objects.all()
+#     return render(req, 'book_user/books.html', {'books': books})
+
+class BooksView(View):
+    def get(seft, req):
+        booklist = Book.objects.all()
+        paginator = Paginator(booklist, 6)
+        pageNumber = req.GET.get('page')
+        try:
+            book = paginator.page(pageNumber)
+        except PageNotAnInteger:
+            book = paginator.page(1)
+        except EmptyPage:
+            book = paginator.page(paginator.num_pages)
+        
+        return render(req, 'book_user/books.html', {'books': book})
+
+class BooksViewSearch(View):
+    def get(self, req):
+        query = req.GET.get('s')
+        if query != None:
+            booklist = Book.objects.filter(name__icontains = query)
+        else:
+            booklist = Book.objects.all()
+        paginator = Paginator(booklist, 6)
+        pageNumber = req.GET.get('page')
+        try:
+            book = paginator.page(pageNumber)
+        except PageNotAnInteger:
+            book = paginator.page(1)
+        except EmptyPage:
+            book = paginator.page(paginator.num_pages)
+        
+        return render(req, 'book_user/books.html', {'books': book})
+        
